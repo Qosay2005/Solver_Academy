3d87cf48a0eec561c3e64b07adc02ab0cf80d6ea
qosay <qlalwheq123@gmail.com>
2026-07-29 00:59:36 -0700

    add a logic for registers operation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export default function Register() {
+  const { register, handleSubmit } = useForm();
+
+  const RegisterForm = async (data) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BURL}/auth/Account/Register`,
+        data
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Register</div>
-  )
+    <Box>
+      <Typography variant="h4">
+        Register
+      </Typography>
+
+      <form onSubmit={handleSubmit(RegisterForm)}>
+        <TextField
+          {...register("userName")}
+          label="User Name"
+          variant="outlined"
+        />
+
+        <TextField
+          {...register("fullName")}
+          label="Full Name"
+          variant="outlined"
+        />
+
+        <TextField
+          {...register("email")}
+          label="Email"
+          variant="outlined"
+        />
+
+        <TextField
+          {...register("password")}
+          label="Password"
+          variant="outlined"
+          type="password"
+        />
+
+        <TextField
+          {...register("phoneNumber")}
+          label="Phone Number"
+          variant="outlined"
+        />
+
+        <Button type="submit" variant="contained">
+          Register
+        </Button>
+      </form>
+    </Box>
+  );
 }
